8765463d3b3083cbca7df11595ba0015875d806c
Austen Rhyce Erickson <a.r.erickson.28@gmail.com>
2021-08-23 19:57:29 -0500

    can successfully render homepage

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,11 +1,38 @@
 const router = require("express").Router();
+const { Post, User } = require('../models');
+const withAuth = require('../utils/auth');
 
 router.get('/', async (req, res) => {
-    try {
-        res.render("homepage")
-    } catch (err) {
-        res.status(500).json(err)
+    try { 
+      const postData = await Post.findAll({
+          include: [
+            {
+                model: User,
+                attributes: ["user_name"]
+            },
+        ],
+    });
+    //   res.status(200).json(postData)
+    const posts = postData.map((post) => post.get({ plain: true }));
+
+    res.render("homepage", {
+        posts, 
+        logged_in: req.session.logged_in 
+    })
+    } catch(err) {
+      res.status(500).json(err)
     }
-})
+  });
+
+// router.get('/', async (req, res) => {
+//     try {
+//         res.render("homepage", {
+//             posts, 
+//             logged_in: req.session.logged_in 
+//         })
+//     } catch (err) {
+//         res.status(500).json(err)
+//     }
+// })
 
 module.exports = router
